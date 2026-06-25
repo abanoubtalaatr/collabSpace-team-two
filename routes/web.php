c51b406e5bc8dc428282e8cbae880278adfb1887
yousef <yousefmohamedmehrez@gmail.com>
2026-06-25 20:44:31 +0300

    i test file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reports', [ReportsController::class, 'index'])->name('reports.index');
